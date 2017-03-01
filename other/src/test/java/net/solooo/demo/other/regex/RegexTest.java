3984b38214304b59ab5de3501a53927ddca7316a
solooo <pjian108@gmail.com>
2017-03-01 14:38:07 +0800

    正则测试

--- other/src/test/java/net/solooo/demo/other/regex/RegexTest.java
+++ other/src/test/java/net/solooo/demo/other/regex/RegexTest.java
@@ -3,6 +3,8 @@ package net.solooo.demo.other.regex;
 import net.sf.jsqlparser.JSQLParserException;
 import net.sf.jsqlparser.parser.CCJSqlParserUtil;
 import net.sf.jsqlparser.statement.Statement;
+import net.sf.jsqlparser.statement.select.Select;
+import net.sf.jsqlparser.util.TablesNamesFinder;
 import org.junit.Test;
 
 import java.util.ArrayList;
@@ -22,28 +24,56 @@ import java.util.regex.Pattern;
 public class RegexTest {
 
     @Test
+    public void test1() {
+        String limitRegex = "limit\\s+\\d+\\s*(?:,\\s*\\d+|)";
+        Pattern p = Pattern.compile(limitRegex);
+        String sql = "select * from pj limit 5  , 56    ";
+        System.out.println(sql.split("limit")[0]);
+        Matcher matcher = p.matcher(sql);
+        if (matcher.find()) {
+            System.out.println(matcher.group());
+            sql = sql.replaceAll(limitRegex, "");
+            System.out.println(sql);
+        }
+
+    }
+
+    @Test
     public void getText() throws JSQLParserException {
-        Pattern useDbPattern = Pattern.compile("use(.*)");
-        String sql = "uSE Ht_test    \n\r;  \n    seLEcT  \n *  \n\r  \t FrOm     pj p WhEre" +
+        Pattern useDbPattern = Pattern.compile("use (.*)");
+        String sql = "use   \n\r    ht_test;  \n    seLEcT  \n *  \n\r  \t FrOm     pj p WhEre" +
                 " id=0 and name=(selECt t FROm t0 t wheRE t.id=p.tid)     Group bY name  ;" +
                 "select t1,   t2,    t3    , t5 from     t0 ;";
         String[] sqls = sql.split(";");
         List<String> list = new ArrayList<>();
         if (sqls.length > 0) {
-            for (String s : sqls) {
-                s = s.trim().toLowerCase();
+            for (int i = 0; i < sqls.length; i++) {
+                String s = sqls[i];
+                s = s.trim().toLowerCase().replaceAll("\\s+", " ");
                 Matcher matcher = useDbPattern.matcher(s);
                 if (matcher.find()) {
-                    list.add(s.replaceAll("\\s+", " "));
+                    list.add(s);
                 } else {
+                    if (i == 0) {
+                        list.add("use ht_test");
+                    }
                     Statement stmt = CCJSqlParserUtil.parse(s);
+                    if (stmt instanceof Select) {
+                        Select select = (Select) stmt;
+                        TablesNamesFinder tn = new TablesNamesFinder();
+                        List<String> tableList = tn.getTableList(select);
+
+                    }
+
                     list.add(stmt.toString());
+
                 }
             }
         }
+
         StringBuilder sb = new StringBuilder();
         for (String str : list) {
-            sb.append(str).append(";");
+            sb.append(str).append("; ");
         }
         System.out.println(sb.toString());
 
