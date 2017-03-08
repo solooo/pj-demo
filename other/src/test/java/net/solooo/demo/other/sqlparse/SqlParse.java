a717fc0a0789ef52ada8499b9c78bc4bc9b0e398
solooo <pjian108@gmail.com>
2017-03-08 13:44:32 +0800

    sql parser

--- other/src/test/java/net/solooo/demo/other/sqlparse/SqlParse.java
+++ other/src/test/java/net/solooo/demo/other/sqlparse/SqlParse.java
@@ -4,7 +4,14 @@ import net.sf.jsqlparser.JSQLParserException;
 import net.sf.jsqlparser.parser.CCJSqlParserUtil;
 import net.sf.jsqlparser.statement.Statement;
 import net.sf.jsqlparser.statement.select.Select;
+import net.sf.jsqlparser.util.TablesNamesFinder;
 import org.junit.Test;
+
+import java.text.MessageFormat;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 /**
  * Title:
@@ -28,5 +35,193 @@ public class SqlParse {
         } catch (JSQLParserException e) {
             e.printStackTrace();
         }
+    }
+
+
+    @Test
+    public void getText() throws JSQLParserException {
+        Pattern useDbPattern = Pattern.compile("use (.*)");
+        String sql = "use   \n\r    ht_test;  \n    seLEcT  \n *  \n\r  \t FrOm     pj p WhEre" +
+                " id=0 and name=(selECt t FROm t0 t wheRE t.id=p.tid)     Group bY name  ;" +
+                "select t1,   t2,    t3    , t5 from     t0 ;";
+        String[] sqls = sql.split(";");
+        List<String> list = new ArrayList<>();
+        if (sqls.length > 0) {
+            for (int i = 0; i < sqls.length; i++) {
+                String s = sqls[i];
+                s = s.trim().toLowerCase().replaceAll("\\s+", " ");
+                Matcher matcher = useDbPattern.matcher(s);
+                if (matcher.find()) {
+                    System.out.println("dbname : " + matcher.group(1) + "  " + matcher.group());
+                    list.add(s);
+                } else {
+                    if (i == 0) {
+                        list.add("use ht_test");
+                    }
+                    Statement stmt = CCJSqlParserUtil.parse(s);
+                    if (stmt instanceof Select) {
+                        Select select = (Select) stmt;
+                        TablesNamesFinder tn = new TablesNamesFinder();
+                        List<String> tableList = tn.getTableList(select);
+
+                    }
+
+                    list.add(stmt.toString());
+
+                }
+            }
+        }
+
+        StringBuilder sb = new StringBuilder();
+        for (String str : list) {
+            sb.append(str).append("; ");
+        }
+        System.out.println(sb.toString());
+
+    }
+
+
+    @Test
+    public void buildSql() {
+        String sql = "   use    ht_test  ; set transaction.type=inceptor; set plsqlUseSlash true;\n " +
+                "create or replace procedure example_proc()\n" +
+                "\tis\n" +
+                "\tdeclare\n" +
+                "\tcnt int:=0;\n" +
+                "\tBEGIN\n" +
+                "\tbegin transaction;\n" +
+                "\tupdate kx.cbcbcbc set value='gaojianxun' where key=1;\n" +
+                "\tcommit;\n" +
+                "\tbegin transaction;\n" +
+                "\tupdate kx.cbcbcbc set value='gaojianxun1' where key=1;\n" +
+                "\tcommit;\n" +
+                "\tEXCEPTION\n" +
+                "\tWHEN others THEN\n" +
+                "\trollback;\n" +
+                "\tdbms_output.put_line('action faild');\n" +
+                "\tEND;";
+        String sql2 = "use    ht_test  ;select * from pj;select * from tt";
+        String sql3 = "use ht_test; CREATE OR REPLACE PACKAGE BODY testpackage12 \n" +
+                "\tIS\n" +
+                "\tv_record_type record_type; \n" +
+                "\tsqlcur cur; \n" +
+                "\tPROCEDURE testprocedure() IS \n" +
+                "\tBEGIN\n" +
+                "\t\tOPEN sqlcur FOR\n" +
+                "\t\tSELECT key,value\n" +
+                "\t\tFROM cbcbcbc\n" +
+                "\t\tWHERE key=1; \n" +
+                "\t\tLOOP\n" +
+                "\t\tFETCH sqlcur INTO v_record_type; \n" +
+                "\t\tEXIT WHEN sqlcur%notfound ; \n" +
+                "\t\tdbms_output.put_line(v_record_type.key||' '||v_record_type.value); \n" +
+                "\t\tEND LOOP;\n" +
+                "\t\tCLOSE sqlcur; \n" +
+                "\tEND;\n" +
+                "\tEND;";
+        String databaseName = "ht_test";
+        int start = 0;
+        int pageSize = 10;
+
+        Pattern useDbPattern = Pattern.compile("^use (.*)");
+        Pattern setPattern = Pattern.compile("^set (.*)");
+        Pattern selectPattern = Pattern.compile("^select (.*)");
+
+        List<String> sqlList = new ArrayList<>();
+        List<SearchBean> searchList = new ArrayList<>();
+        String useStatement;
+        String execSql = "";
+        String[] sqls = sql3.split(";");
+        for (String s : sqls) {
+            String str = s.replaceAll("\\s+", " ").trim();
+            Matcher useMatcher = useDbPattern.matcher(str);
+            Matcher setMatcher = setPattern.matcher(str);
+            Matcher selectMatcher = selectPattern.matcher(str);
+
+            if (useMatcher.find()) { // use 语句处理
+                useStatement = useMatcher.group();
+                sqlList.add(useStatement);
+            } else if (setMatcher.find()) { // set 属性设置处理
+                sqlList.add(setMatcher.group());
+            } else if (selectMatcher.find()) { // select 查询语句
+                SearchBean searchBean = new SearchBean(databaseName, selectMatcher.group(), start, pageSize);
+                searchList.add(searchBean);
+            } else { // create 创建语句
+                execSql += str + "; ";
+            }
+        }
+        sqlList.add(execSql);
+
+        for (Object obj : sqlList) {
+            if (obj instanceof String) {
+                System.out.println(obj);
+            } else if (obj instanceof SearchBean) {
+                System.out.println("-----------------------------");
+                System.out.println("sql:      "+ ((SearchBean) obj).getSql());
+                System.out.println("countSql: "+ ((SearchBean) obj).getCountSql());
+                System.out.println("-----------------------------");
+            }
+        }
+    }
+
+    private class ExecBean {
+        private List<String> sql;
+
+        private List<SearchBean> searchBeens;
+
+        public ExecBean(List<String> sql, List<SearchBean> searchBeens) {
+            this.sql = sql;
+            this.searchBeens = searchBeens;
+        }
+
+        public List<String> getSql() {
+            return sql;
+        }
+
+        public void setSql(List<String> sql) {
+            this.sql = sql;
+        }
+
+        public List<SearchBean> getSearchBeens() {
+            return searchBeens;
+        }
+
+        public void setSearchBeens(List<SearchBean> searchBeens) {
+            this.searchBeens = searchBeens;
+        }
+    }
+
+    /**
+     * 保存sql处理结果
+     */
+    private class SearchBean {
+
+        private int start;
+
+        private int pageSize;
+
+        private String databaseName;
+
+        private String sql;
+
+        public SearchBean(String databaseName, String sql, int start, int pageSize) {
+            this.databaseName = databaseName;
+            this.sql = sql;
+            this.start = start;
+            this.pageSize = pageSize;
+        }
+
+        public String getCountSql() {
+
+            return MessageFormat
+                    .format("USE {0};SELECT COUNT(1) FROM ({1})", databaseName, sql);
+        }
+
+        public String getSql() {
+            return MessageFormat
+                    .format("USE {0};SELECT * FROM ({1}) LIMIT {2},{3}",
+                            databaseName, sql, start, pageSize);
+        }
+
     }
 }
