9e5b3a21ac70c1d569500aeb932abb401fb14da6
solooo <pjian108@gmail.com>
2017-04-27 15:40:58 +0800

    加密

---
+++ other/src/test/java/net/solooo/demo/other/security/ZipUtil.java
@@ -0,0 +1,58 @@
+package net.solooo.demo.other.security;
+
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.util.zip.GZIPInputStream;
+import java.util.zip.GZIPOutputStream;
+
+/**
+ * Description:
+ * Author:Eric
+ * Date:2017/4/27
+ */
+public class ZipUtil {
+
+    // 压缩
+    public static String compress(String str) throws IOException {
+        if (str == null || str.length() == 0) {
+            return str;
+        }
+        ByteArrayOutputStream out = new ByteArrayOutputStream();
+        GZIPOutputStream gzip = new GZIPOutputStream(out);
+        gzip.write(str.getBytes());
+        gzip.close();
+        return out.toString("utf-8");
+    }
+
+    // 解压缩
+    public static String uncompress(String str) throws IOException {
+        if (str == null || str.length() == 0) {
+            return str;
+        }
+        ByteArrayOutputStream out = new ByteArrayOutputStream();
+        ByteArrayInputStream in = new ByteArrayInputStream(str
+                .getBytes("utf-8"));
+        GZIPInputStream gunzip = new GZIPInputStream(in);
+        byte[] buffer = new byte[256];
+        int n;
+        while ((n = gunzip.read(buffer)) >= 0) {
+            out.write(buffer, 0, n);
+        }
+        // toString()使用平台默认编码，也可以显式的指定如toString("GBK")
+        return out.toString();
+    }
+
+    // 测试方法
+    public static void main(String[] args) throws IOException {
+
+        //测试字符串
+        String str="%5B%7B%22lastUpdateTime%22%3A%222011-10-28+9%3A39%3A41%22%2C%22smsList%22%3A%5B%7B%22liveState%22%3A%221";
+
+        System.out.println("原长度："+str.length());
+
+        System.out.println("压缩后："+ZipUtil.compress(str));
+
+        System.out.println("解压缩："+ZipUtil.uncompress(ZipUtil.compress(str)));
+    }
+}
