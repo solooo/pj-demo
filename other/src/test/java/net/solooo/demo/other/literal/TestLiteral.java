3b74072c8f6183677e9130e5afe73b61919f337f
solooo <pjian108@gmail.com>
2017-05-10 16:25:12 +0800

    匹配引号外面的内容，并转换大小写

--- other/src/test/java/net/solooo/demo/other/literal/TestLiteral.java
+++ other/src/test/java/net/solooo/demo/other/literal/TestLiteral.java
@@ -2,6 +2,9 @@ package net.solooo.demo.other.literal;
 
 import org.junit.Test;
 
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 /**
  * Description:
  * Author:Eric
@@ -24,5 +27,28 @@ public class TestLiteral {
     public void asciiToString() {
         int a= 97;
         System.out.println((char) a);
+    }
+
+    @Test
+    public void index() {
+        String val = ",index,name";
+        System.out.println(val.indexOf("."));
+    }
+
+    /**
+     * 匹配引号外面的内容
+     */
+    @Test
+    public void regex() {
+        String str = "SELECT * from car_data_hbase_index_new_copy where id =  '20170502022403_云BE9213' AND 1=1 AND id = '123ABCS'";
+        System.out.println(str);
+        Matcher matcher = Pattern.compile("[A-Z](?=([^\\\']*\\\'[^\\\']*\\\')*[^\\\']*$)").matcher(str);
+
+        StringBuffer sb = new StringBuffer();
+        while(matcher.find()){
+            matcher.appendReplacement(sb, matcher.group().toLowerCase());
+        }
+        matcher.appendTail(sb);
+        System.out.println(sb.toString());
     }
 }
