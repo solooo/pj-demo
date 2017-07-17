5150ad3d5339d258459c265e08896a1a077e7f20
solooo <pjian108@gmail.com>
2017-07-17 20:35:53 +0800

    sha1 test

--- other/src/test/java/net/solooo/demo/other/literal/TestLiteral.java
+++ other/src/test/java/net/solooo/demo/other/literal/TestLiteral.java
@@ -5,6 +5,7 @@ import org.junit.Test;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.security.MessageDigest;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
@@ -102,4 +103,34 @@ public class TestLiteral {
         System.out.println("peijian".hashCode());
     }
 
+    @Test
+    public void sha1Test() {
+        System.out.println(getSha1("this is test"));
+    }
+
+    public static String getSha1(String str){
+        if(str==null||str.length()==0){
+            return null;
+        }
+        char hexDigits[] = {'0','1','2','3','4','5','6','7','8','9',
+                'a','b','c','d','e','f'};
+        try {
+            MessageDigest mdTemp = MessageDigest.getInstance("SHA1");
+            mdTemp.update(str.getBytes("UTF-8"));
+
+            byte[] md = mdTemp.digest();
+            int j = md.length;
+            char buf[] = new char[j*2];
+            int k = 0;
+            for (int i = 0; i < j; i++) {
+                byte byte0 = md[i];
+                buf[k++] = hexDigits[byte0 >>> 4 & 0xf];
+                buf[k++] = hexDigits[byte0 & 0xf];
+            }
+            return new String(buf);
+        } catch (Exception e) {
+            // TODO: handle exception
+            return null;
+        }
+    }
 }
